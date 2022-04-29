594c9843b8c73f8cbae1fa88ea78e9995c25b090
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-04-29 17:51:46 -0300

    Lista de lembretes funcionando

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+
+import { 
+  Button,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+{/*
+  Observações:
+  felxDirection: Row ou Colum
+  justifyContent opera no eixo principal
+  alingItems = secundario/outro eixo  
+  
+  <ScrollView> <- não recicla os componentes
+        Lista de lembretes
+
+        {
+          lembretes.map((x) => (
+            <View key={x} style={styles.itemNaLista}>
+              <Text>{x}</Text>
+            </View>
+          ))
+        }
+
+  </ScrollView>
+
+*/}
 
 export default function App() {
+  const [lembrete, setLembrete] = useState('')
+  const [lembretes, setLembretes] = useState([])
+  const [contadorLembretes, setContadorLembrete] = useState(0)
+
+  const capturarLembrete = (LembreteDigitado) => {
+    setLembrete(LembreteDigitado)
+  }
+
+  const adicionarLembrete = () =>{
+    setLembretes(lembretes => {
+      setContadorLembrete(contadorLembretes + 1)
+      return [{key: contadorLembretes.toString(), value: lembrete}, ...lembretes]
+    })
+
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.telaPrincipalView}>
+      
+      <View>
+      {/*Usuario insere lembrete aqui*/}
+        <TextInput
+        onChangeText={capturarLembrete}
+          placeholder='Lembrar...'
+          style={{borderBottomColor: 'Black', borderBottomWidth: 1, marginBottom: 8, padding: 12}}
+
+        />
+        <Button 
+          title="Adicionar Lembrete"
+          onPress={adicionarLembrete}
+        />
+      </View>
+
+      <FlatList
+        data={lembretes}
+        renderItem={
+          l =>(
+            <View style={styles.itemNaLista}>
+              <Text>{l.item.value}</Text>
+            </View>
+          )
+        }
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  telaPrincipalView: {
+    padding: 15
   },
+  itemNaLista:{
+    padding: 10,
+    marginTop: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 8,
+    textAlign: 'center',
+    backgroundColor: '#CCC'
+    
+  }
 });
